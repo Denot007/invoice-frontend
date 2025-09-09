44e1692d3decc61efe1490905af8aad597e05e80
Denot007 <denot007@icloud.com>
2025-09-09 15:14:47 -0400

    adding https://invoyci.onrender.com

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL ; 
+// const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api'; 
+const API_BASE_URL = 'https://invoyci.onrender.com/api'; 
 
 class ApiService {
   constructor() {
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL ;
+const API_BASE_URL =  'https://invoyci.onrender.com/api';
+// const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api';
 
 
-
+// https://invoyci.onrender.com
 class AuthService {
   constructor() {
     this.client = axios.create({
