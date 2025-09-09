09458a730493c52cf7a5e12c7488898c0d3f942f
Denot007 <denot007@icloud.com>
2025-09-09 12:00:55 -0400

    added .env

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = "invoyci.netlify.app/api"
-const _API_BASE_URL_local = 'http://localhost:8000/api'; 
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api'; 
 
 class ApiService {
   constructor() {
